e1dd9542638b228e581ea660531de57c7a2f4044
suresh <suresh.avlm@gmail.com>
2018-10-21 21:40:28 +0530

    initial check-ins

---
+++ app.js
@@ -0,0 +1,17 @@
+
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT | 8080;
+
+app.listen(port, function() {
+	console.log("Server running on port %s", port);
+});
+
+/* http://localhost:8080 */
+app.get("/", function(req, res) {
+
+	res.sendFile(__dirname + '/index.html');
+
+});
